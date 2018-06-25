075e08911c3cdfcac081354c292ca2320802079a
Lawrence Splaver <lsplaver@yahoo.com>
2018-06-25 17:49:47 -0400

    Updated to ES6 format

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const app = express();
 const parser = require ("body-parser")
-const db = require("./models");
+const db = require("./models").default;
 
 const routes = require ("./routes")
 const PORT = process.env.port || 3000
