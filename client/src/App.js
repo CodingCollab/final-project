bcc742cd881c2829808ad6f426205d7442e39426
Kevin J Glasow <kevin.glasow@gmail.com>
2018-06-26 19:45:23 -0400

    one component mostly working!

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Homepage from "./components/Homepage";
+import Header from "./components/Header";
 
-const App = () => <Homepage />;
+const App = () => <Header />;
 
 export default App;
