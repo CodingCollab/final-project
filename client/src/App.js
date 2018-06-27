878d8d25372c783b4d686369a0a66d156b33f932
Kevin J Glasow <kevin.glasow@gmail.com>
2018-06-26 21:19:42 -0400

    login component working with no css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 const App = () => [
     <Header />,
-    <div></div>,
+    <Login />,
     <Footer />    
 ];
 
