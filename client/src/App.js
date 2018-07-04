3cf101a976118cc5a4d892c3d46f2d13df23b231
Matthew Skrajner <matthew.skrajner@gmail.com>
2018-07-04 14:50:59 -0400

    added components and resized thumbnails

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 // import Header from "./components/Header";
 // import Login from "./components/Login";
 // import Footer from "./components/Footer";
+// import Card from "./components/Card";
+// import Jumbotron from "./components/Jumbo";
 import NewRequestForm from "./components/NewRequestForm";
 
 const App = () => [
     // <Header />,
+    // <p><Jumbotron /></p>,
+    // <p><Card /></p>
     // <Footer />    
     <NewRequestForm />
 ];
---
+++ client/src/components/Jumbo/Jumbo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+const Jumbo = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">CodingCollab</h1>
+          <p className="lead">Write some code and earn some cash - for you and for charity!</p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Jumbo;
