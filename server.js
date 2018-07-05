05acd3445e6180f725426004a250b6262503f63f
Lawrence Splaver <lsplaver@yahoo.com>
2018-07-04 21:33:31 -0400

    Correctly working environment variable for config

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require ("express");
 const app = express();
 const parser = require ("body-parser");
 const db = require("./models"); //.default;
-const path = require("path")
-const env = require("if-env").load();
+const path = require("path");
+// const env = require("dotenv"); //.config("./config/config.js", "config.js"); //.load();
+const env = require("dotenv");
 
+env.load(path.resolve('./config/config', 'config.js'));
 const routes = require ("./routes");
 const PORT = process.env.port || 3001;
 
