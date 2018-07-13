d3ed9b8017b213e013786c77f934d7360f63442b
Matthew Skrajner <32440484+MattSkrajner@users.noreply.github.com>
2018-07-12 20:35:01 -0400

    minor typo tweaks

---
+++ src/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from '../../client/src/components/Pages/htmlRoutes'
+
+const App = () => [
+    <Header />,
+    <Main />,
+    <Footer />
+];
+
+export default App;
+
+
---
+++ src/src/components/About/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render () {
+      return (
+        
+        <div className="home">
+            <p>CodingCollab is a full stack web app built using React, Sequalize, Node as well as other 
+              technologies. The designers (Lawrence S., Kevin G., Matt S., and Brian Z.) have built 
+              this as a project in their coding bootcame.</p>
+
+            <p>This app is built to allow a community of coders to meet with users who need their 
+               specialty in a specific language. Users can post new requests for specialists in a 
+               specific coding language or sign up to complete requests in a language they're familiar
+               with.</p>
+        </div>
+        );
+    }
+  };
+
+export default About;
