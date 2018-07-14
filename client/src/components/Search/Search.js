949655ef969b44837f3133c246f5ed105563ecb5
Kevin J Glasow <kevin.glasow@gmail.com>
2018-07-14 11:56:01 -0400

    working search form

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,3 +1,11 @@
+// Last Update by Kevin Glasow on 07/14/2018
+
+// *********************************************************************************
+// Search.js - component to contain serch logic and rendering
+// *********************************************************************************
+
+// DEPENDENCIES
+// =============================================================
 import React, { Component } from "react";
 import {
   InputGroup,
@@ -10,10 +18,13 @@ import {
   DropdownItem
  } from 'reactstrap';
 
+// DEPENDENCIES
+// =============================================================
+// Setting empty state for the componenet 
 class searchForm extends Component {
   state = {
     searchType: "",
-    searchText: ""
+    searchTerm: ""
   };
 
 
@@ -24,7 +35,9 @@ class searchForm extends Component {
     this.toggleSplit = this.toggleSplit.bind(this);
     this.state = {
       dropdownOpen: false,
-      splitButtonOpen: false
+      splitButtonOpen: false,
+      searchType: "Search Type",
+      searchTerm: ""
     };
   }
 
@@ -40,15 +53,35 @@ class searchForm extends Component {
     });
   }
 
+  handleDropdownSelect = event => {
+    let value = event.target.value;
+    const name = event.target.name;
+    
+    console.log(name);
+    console.log(value);
+    // Updating the input's state
+    this.setState({
+      searchType: value
+      // [name]: value
+    });
+
+    console.log(this.state.searchType);
+  }
+
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
     let value = event.target.value;
     const name = event.target.name;
-
+    if(name==="searchType"){
+      console.log("here");
+      document.getElementById("header").innerHTML = value;
+    }
     // Updating the input's state
     this.setState({
       [name]: value
     });
+
+    console.log(this.state);
   };
 
   handleFormSubmit = event => {
@@ -58,30 +91,40 @@ class searchForm extends Component {
     console.log(this.state)
 
     this.setState({
-      searchType: "",
-      searchText: ""
+      searchType: "Search Type",
+      searchTerm: ""
       });
+
+
   };
 
-
+// RENDERING COMPONENT
+// =============================================================
   render() {
+    console.log(this.state);
     return (
       <div>
         <InputGroup>
           <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
             <DropdownToggle caret>
-              Search Type 
+              {this.state.searchType} 
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem header>Search Type</DropdownItem>
-              <DropdownItem>User Name</DropdownItem>
-              <DropdownItem>Request Description</DropdownItem>
-              <DropdownItem>Language</DropdownItem>
+              <DropdownItem header value={this.state.searchType}>Search Type</DropdownItem>
+              <DropdownItem onClick={this.handleDropdownSelect} value="User Name" name="searchType">User Name</DropdownItem>
+              <DropdownItem onClick={this.handleDropdownSelect} value="Request Description" name="searchType">Request Description</DropdownItem>
+              <DropdownItem onClick={this.handleDropdownSelect} value="Language" name="searchType">Language</DropdownItem>
               <DropdownItem divider />
-              <DropdownItem>All</DropdownItem>
+              <DropdownItem onClick={this.handleDropdownSelect} value="All" name="searchType">All</DropdownItem>
             </DropdownMenu>
           </InputGroupButtonDropdown>
-          <Input placeholder="Search Term" />
+          <Input 
+            value={this.state.searchTerm}
+            name="searchTerm"
+            onChange={this.handleInputChange}
+            type="text"
+            // placeholder="Search Term"
+          />
           <InputGroupAddon addonType="append"><Button color="secondary" onClick={this.handleFormSubmit}>Search</Button></InputGroupAddon>
         </InputGroup>
       </div>
