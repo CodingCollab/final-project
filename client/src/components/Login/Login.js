f889a0961d7f61544b2218909293af80f77d2f54
Kevin J Glasow <kevin.glasow@gmail.com>
2018-07-14 13:39:05 -0400

    login bug fixed

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,60 +1,81 @@
 import React, { Component } from "react";
-import { form } from "reactstrap";
-import "../Pages/style.css"
+import {
+  Form,
+  Input,
+  Button,
+ } from 'reactstrap';
 
 class Login extends Component {
-    render () {
+  state = {
+    userName: "",
+    password: ""
+  };
+
+    handleInputChange = event => {
+      // Getting the value and name of the input which triggered the change
+      let value = event.target.value;
+      const name = event.target.name;
+  
+      // Updating the input's state
+      this.setState({
+        [name]: value
+      });
+    };
+
+    handleFormSubmit = event => {
+
+      event.preventDefault()
+      // logic
+      console.log(this.state)
+  
+      this.setState({
+        userName: "",
+        password: ""
+      });
+    };
+
+  render() {
       return (
- 
-        <div className="limiter">
-          <div className="container-login100">
-            <div className="wrap-login100">
-              <div className="login100-pic js-tilt" data-tilt>
-                <img src="images/img-01.jpg" alt="IMG" />
-              </div>
-              <form className="login100-form validate-form">
-                <span className="login100-form-title">
-                  Member Login
-                </span>
-                <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                  <input className="input100" type="text" name="email" placeholder="Email" />
-                  <span className="focus-input100" />
-                  <span className="symbol-input100">
-                    <i className="fa fa-envelope" aria-hidden="true" />
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Password is required">
-                  <input className="input100" type="password" name="pass" placeholder="Password" />
-                  <span className="focus-input100" />
-                  <span className="symbol-input100">
-                    <i className="fa fa-lock" aria-hidden="true" />
-                  </span>
-                </div>
-                <div className="container-login100-form-btn">
-                  <button className="login100-form-btn">
-                    Login
-                  </button>
-                </div>
-                <div className="text-center p-t-12">
-                  <span className="txt1">
-                    Forgot
-                  </span>
-                  <a className="txt2" href="/lostPass">
-                    Username / Password?
-                  </a>
-                </div>
-                <div className="text-center p-t-136">
-                  <a className="txt2" href="/NewUserForm">
-                    Create your Account
-                    <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true" />
-                  </a>
-                </div>
-              </form>
-            </div>
+        <div>
+        <Form className="form">
+          <input
+            value={this.state.userName}
+            name="userName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="User Name"
+          />
+          <Input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />      
+          <div className="container-login100-form-btn">
+             <Button className="login100-form-btn" onClick={this.handleFormSubmit}>
+                Login
+             </Button>
+          </div>
+        </Form>
+
+          <div className="text-center p-t-12">
+            <span className="txt1">
+              Forgot
+            </span>
+            <a className="txt2" href="/lostPass">
+              Username / Password?
+            </a>
+          </div>
+          <div className="text-center p-t-136">
+            <a className="txt2" href="/NewUserForm">
+              Create your Account
+              <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true" />
+             </a>
           </div>
         </div>
       );
-    }
-  };
+}
+}
 
 export default Login;
