b4ee67dccca0fd0b0f7499134bf65df72dbe1544
Matthew Skrajner <matthew.skrajner@gmail.com>
2018-07-16 22:02:54 -0400

    updated about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,14 +5,18 @@ class About extends Component {
       return (
         
         <div className="home">
-            <p>CodingCollab is a full stack web app built using React, Sequalize, Node as well as other 
+            <h1>About CodeCollab</h1>
+            <br></br>
+            <p>CodeCollab is a full stack web app built using React, Sequalize and Node, as well as other 
               technologies. The designers (Lawrence S., Kevin G., Matt S., and Brian Z.) have built 
-              this as a project in their coding bootcame.</p>
+              this as their final project in Case Western Reserve University's Coding Bootcamp.</p>
 
-            <p>This app is built to allow a community of coders to meet with users who need their 
+            <p>This app allows a community of coders to meet with users who need their 
                specialty in a specific language. Users can post new requests for specialists in a 
                specific coding language or sign up to complete requests in a language they're familiar
-               with.</p>
+               with. Users can also put a small dollar amount on their request to encourage more high quality solutions.</p>
+               
+               <p><a href="https://github.com/CodingCollab"> The GitHub repository can be viewed here. </a></p>
         </div>
         );
     }
