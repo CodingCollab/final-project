aa4c0fbbc107366c7cd88f1de6115e40a9e9f56a
Kevin J Glasow <kevin.glasow@gmail.com>
2018-07-18 20:02:46 -0400

    server.js given static path

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const path = require("path");
 // const env = require("dotenv"); //.config("./config/config.js", "config.js"); //.load();
 const env = require("dotenv");
 
+app.use(express.static('client/build'))
+
 env.load(path.resolve('./config/config', 'config.js'));
 const routes = require ("./routes");
-const homePath = require("./client/build/index.html");
 const PORT = process.env.PORT || 3001;
 
 app.use(parser.urlencoded({extended: false}));
 app.use(parser.json());
 app.use(routes);
-app.get(homePath);
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // app.get("*", res.sendfile(path.join(__dirname, "./client/public/index.html")) );`
 // console.log(process.env)
